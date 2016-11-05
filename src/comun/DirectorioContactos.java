1852aee72ebaaa04c72fd0325305937d38833084
ziroz <ziroz155@gmail.com>
2016-11-05 17:11:52 -0500

    configuracion inicial

---
+++ src/comun/DirectorioContactos.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comun;
+
+import abb.NodoBinario;
+
+/**
+ *
+ * @author Ciro
+ */
+public class DirectorioContactos extends abb.ABB {
+    
+    public Contacto BuscarContactoNombre(String nombreContacto) {
+        
+        NodoBinario<Contacto> nodoActual = super.getRaiz();
+        Contacto nodoEncontrado = null;
+                
+        while(nodoActual != null && nodoEncontrado == null)
+        {
+            if(nodoActual.getItem().getNombre().compareTo(nombreContacto) == 0)
+            {
+                nodoEncontrado = nodoActual.getItem();
+            }
+        else
+            
+            if(nodoActual.getItem().getNombre().compareTo(nombreContacto) > 0)
+            {
+                if(nodoActual.getHijoIzquierdo() == null){
+                    nodoActual = null;
+                }else{
+                    nodoActual = nodoActual.getHijoIzquierdo();
+                }
+            }else
+            {
+                if(nodoActual.getHijoDerecho()== null){
+                    nodoActual = null;
+                }else{
+                    nodoActual = nodoActual.getHijoDerecho();
+                }
+            }
+        }
+        
+        return nodoEncontrado;
+    }
+    
+    
+    public Contacto BuscarContactoCorreo(String correo) {
+    return BuscarContactoCorreo(correo, super.getRaiz());
+    }
+    
+    private Contacto BuscarContactoCorreo(String correo, NodoBinario<Contacto> nodoRaiz) {
+        
+        if(nodoRaiz != null)
+        {
+            if(nodoRaiz.getItem().getCorreoElectronico().compareTo(correo) == 0)
+            {
+                return nodoRaiz.getItem();
+            }
+
+            Contacto buscarIzquierda = BuscarContactoCorreo(correo, nodoRaiz.getHijoIzquierdo());
+            if(buscarIzquierda != null) return buscarIzquierda;
+
+            Contacto buscarDerecha = BuscarContactoCorreo(correo, nodoRaiz.getHijoDerecho());
+            if(buscarDerecha != null) return buscarDerecha;
+        }
+        return null;
+    }
+}
